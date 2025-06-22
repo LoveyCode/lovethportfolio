a0393fda43bd398fe4fb6bc462ab1157abadc9f7
Loveth <lovazodo@gmail.com>
2025-06-22 14:06:54 +0100

    first commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I emphasize working closely with clients and encouraging transparent communication.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.jpeg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I’m flexible with time zone communication",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "text-white font-sm z-20 justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "I am always seeking ways to grow and enhance my skills",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "text-white justify-center",
+      img: "/b6.jpeg",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "I'm deeply interested in technology and enthusiastic about software development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "text-white justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "I enjoy building software that creates meaningful experiences and real impact.",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: " text-white justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.jpeg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+ 
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "MedIntel: Robust patient appointment system Integrated in Regal Care Hospital website",
+      des: "A comprehensive hospital management system for patient registration, appointments, staff coordination, medical records, and seamless admin control",
+      img: "/p1.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "shadcn.png", "/fm.svg", "react.svg"],
+      link: "https://regal-care-hospital.vercel.app",
+      href: "https://regal-care-hospital.vercel.app"
+    },
+    {
+      id: 2,
+      title: "Zanga - Ecommerce Website",
+      des: "Zanga is a modern ecommerce platform designed for seamless shopping experiences, featuring intuitive navigation, secure checkout, and responsive design.",
+      img: "/p2.png",
+      iconLists: ["react.svg", "Nodejs.png", "/fm.svg"],
+      link: "/https://zanga.app/",
+      href: "/https://zanga.app/"
+    },
+    {
+      id: 3,
+      title: "Headmaster - I am currently building a school management system",
+      des: "Headmaster is a smart, all-in-one school management system designed to simplify administration, track performance, and enhance communication between teachers, students, and parents.",
+      img: "/p3.jpeg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg", "react-native.svg"],
+      link: "/headmaster.com",
+    },
+    {
+      id: 4,
+      title: "Mobo - Currently building a taxi mobile app",
+      des: "Mobo is a reliable taxi app offering fast, safe, and convenient rides at your fingertips anytime, anywhere.",
+      img: "/p4.jpeg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "/mobo.com",
+   
+    },
+  ];
+
+  export const testimonials = [
+    {
+      quote:
+        "Watching Loveth Azodo grow from a curious learner into a passionate programmer has been truly inspiring. At High Target School, we prioritize nurturing talent, and it's a joy to see how our environment helped spark a lasting interest in technology. I’m proud that our school played a role in shaping such a promising future in programming.",
+      name: "Mrs. Ebere Nwoji",
+      title: "Proprietress, High Target School",
+    },
+    {
+      quote:
+        "We needed a professional website designer who could clearly represent what we do, and Loveth Azodo delivered exactly that. We're truly glad to have had her on our team and we wish her even greater heights ahead.",
+      name: "Uchenna Benson",
+      title: "Manager, Patent Global Services",
+    },
+    {
+      quote:
+        "Having Loveth Azodo manage our website has brought a new level of professionalism to Optimum Times Online. She’s reliable, creative, and always quick to respond when updates are needed. Her attention to detail and understanding of our content goals have helped us maintain a clean, user-friendly platform for our readers. We're proud to have her on board..",
+      name: "Clement Nwoji",
+      title: "Publisher, Optimum Times Online",
+    },
+
+    {
+      quote:
+        "Learning programming under Miss Loveth Azodo was a turning point for me. She made complex topics easy to understand and always took time to guide me patiently. Her teaching sparked my interest in tech, and I’m truly grateful for the foundation she helped me build. She’s more than a teacher—she’s a mentor I’ll always look up to.",
+      name: " Ebube Nwabuike",
+      title: "A student",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Website manager and designer",
+      desc: "Assisted in the development of a web-based platform using wordpress, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - Patent Global service",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Assisted in developing a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Tutor",
+      desc: "Taught programming to both senior and junior students, guiding them until they gained confidence and became proficient",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/LoveyCode"
+  },
+  {
+    id: 2,
+    img: "/wha.svg",
+    link: "https://wa.me/2349038507567"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/loveth-azodo-06450426a"
+  },
+];
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div
+        className="h-screen w-full bg-black-100 
+        absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100
+         "
+        />
+      </div>
+    
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <div className="w-[300px] h-[300px] rounded-full overflow-hidden bg-blue-500">
+            <Image
+              src="/assets/images/profilePics.jpg"
+              height={300}
+              width={300}
+              alt="ProfilePics"
+              className="w-full h-full object-cover object-top"
+            />
+          </div>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Turning ideas into scalable and efficient software solutions."
+          />
+
+          <p className="text-center text-white-100 md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I'm Loveth — a passionate React.js/Next.js developer based in
+            Nigeria with a background in Computer Science from Nnamdi Azikiwe
+            University. I specialize in building dynamic and responsive web
+            applications, and I also have experience in programming.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
