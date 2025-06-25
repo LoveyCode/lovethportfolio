ce9c546ee007b20af3018d1d40d81d9da66d9279
Loveth <lovazodo@gmail.com>
2025-06-25 21:41:59 +0100

    adding paddig to recent projects in small screen

--- src/app/components/RecentProjects.tsx
+++ src/app/components/RecentProjects.tsx
@@ -17,7 +17,7 @@ const RecentProjects = () => {
         {projects.map((item) => (
           <div
             onClick={() => window.open(item.href, "_blank")}
-            className="cursor-pointer sm:h-[41rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+            className="cursor-pointer md:py-10 sm:h-[41rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
             key={item.id}
           >
             <PinContainer title={item.link} href={item.href}>
